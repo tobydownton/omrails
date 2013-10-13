469fc7fa3eaea94fac3d35b10ead9811eeb7b3c0
Toby Downton <tobydownton@hotmail.com>
2013-10-13 13:19:20 +0100

    Add image uploading via url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image # This line won't work in Rails 4, instead, you'll have to do what I show below
+  attr_accessible :description, :image, :image_remote_url # This line won't work in Rails 4, instead, you'll have to do what I show below
+
 
   validates :description, presence: true
   validates :user_id, presence: true
@@ -8,5 +9,10 @@ class Pin < ActiveRecord::Base
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
   belongs_to :user
+
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
 
 end
